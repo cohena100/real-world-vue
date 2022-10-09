67626a02af19ef528daa345553bb181ccdc0b771
Avi Cohen <cohena100@gmail.com>
2022-10-09 12:31:34 +0300

    tests

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,23 +1,30 @@
 <template>
   <h1>Events for Good</h1>
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-
+    <router-link
+      class="event-link"
+      :to="{ name: 'EventDetails', params: { id: event.id } }"
+      v-for="event in events"
+      :key="event.id"
+    >
+      <EventCard :event="event" />
+    </router-link>
     <div class="pagination">
       <router-link
         id="page-prev"
         :to="{ name: 'EventList', query: { page: page - 1 } }"
         rel="prev"
         v-if="page != 1"
-        >&#60; Previous</router-link
       >
-
+        &#60; Previous</router-link
+      >
       <router-link
         id="page-next"
         :to="{ name: 'EventList', query: { page: page + 1 } }"
         rel="next"
         v-if="hasNextPage"
-        >Next &#62;</router-link
+      >
+        Next &#62;</router-link
       >
     </div>
   </div>
@@ -61,6 +68,7 @@ export default {
   display: flex;
   width: 290px;
 }
+
 .pagination a {
   flex: 1;
   text-decoration: none;
---
+++ tests/unit/EventCard.spec.js
@@ -0,0 +1,27 @@
+import { mount } from "@vue/test-utils";
+import EventCard from "@/components/EventCard";
+
+describe("EventCard", () => {
+  it("renders event's data successfully", () => {
+    const event = {
+      id: 1,
+      time: "12:00PM",
+      date: "September 29th, 2022",
+      title: "Coaching Little League",
+    };
+    mount(EventCard, {
+      props: {
+        event,
+      },
+    });
+    const wrapper = mount(EventCard, {
+      props: {
+        event,
+      },
+    });
+    const wrapperHtml = wrapper.html();
+    expect(wrapperHtml).toContain(event.date);
+    expect(wrapperHtml).toContain(event.time);
+    expect(wrapperHtml).toContain(event.title);
+  });
+});
